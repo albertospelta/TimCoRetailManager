18eb00df6a0f36847007af5f3be40200ed2f27e0
Alberto Spelta <alberto.spelta@live.com>
2020-09-06 18:10:48 +0200

    Added LoginViewModel properties and binding to user and password boxes

--- src/TRMDesktopUI/ViewModels/LoginViewModel.cs
+++ src/TRMDesktopUI/ViewModels/LoginViewModel.cs
@@ -7,7 +7,46 @@ using System.Threading.Tasks;
 
 namespace TRMDesktopUI.ViewModels
 {
-    public class LoginViewModel
+    public class LoginViewModel : Screen
     {
+        private string _username;
+        private string _password;
+
+        public string Username
+        {
+            get => _username;
+            set 
+            { 
+                _username = value;
+                NotifyOfPropertyChange(() => Username);
+            }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                NotifyOfPropertyChange(() => Password);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+
+        public bool CanLogIn
+        {
+            get
+            {
+                if (Username?.Length > 0 && Password?.Length > 0)
+                    return true;
+
+                return false;
+            }
+        }
+
+        public void LogIn(string username, string password)
+        {
+            Console.WriteLine();
+        }
     }
 }
