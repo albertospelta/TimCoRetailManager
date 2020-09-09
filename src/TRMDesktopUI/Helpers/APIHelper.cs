459955dae47cb5ea5d2f679ba7c3b21f392ecb9d
Alberto Spelta <alberto.spelta@live.com>
2020-09-09 18:12:24 +0200

    Added APIHelper and model AuthenticatedUser classes

---
+++ src/TRMDesktopUI/Helpers/APIHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using TRMDesktopUI.Models;
+
+namespace TRMDesktopUI.Helpers
+{
+    public class APIHelper : IAPIHelper
+    {
+        private HttpClient _client;
+
+        public APIHelper()
+        {
+            InitializeClient();
+        }
+
+        private void InitializeClient()
+        {
+            string api = ConfigurationManager.AppSettings["api"];
+
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri(api);
+            _client.DefaultRequestHeaders.Accept.Clear();
+            _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<AuthenticatedUser> Authenticate(string username, string password)
+        {
+            var content = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("grant_type", "password"),
+                new KeyValuePair<string, string>("username", username),
+                new KeyValuePair<string, string>("password", password),
+            });
+
+            using (var response = await _client.PostAsync("/token", content))
+            {
+                if (response.IsSuccessStatusCode == false)
+                    throw new Exception(response.ReasonPhrase);
+
+                var result = await response.Content.ReadAsAsync<AuthenticatedUser>();
+                return result;
+            }
+        }
+    }
+}
