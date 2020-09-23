670878255d5bac15a5654648f559c9910d30c71e
Alberto Spelta <alberto.spelta@live.com>
2020-09-23 20:39:00 +0200

    SQL - added data to table Product, added sp to GetAll data from table

---
+++ src/TRMData/dbo/Stored Procedures/spProductGetAll.sql
@@ -0,0 +1,16 @@
+﻿CREATE PROCEDURE [dbo].[spProductGetAll]
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT 
+		p.[Id],
+		p.[ProductName],
+		p.[Description],
+		p.[RetailPrice],
+		p.[QuantityInStock]
+	FROM 
+		[dbo].[Product] AS p
+	ORDER BY
+		p.[ProductName]
+END
